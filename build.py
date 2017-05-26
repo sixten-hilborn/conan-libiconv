47dd4d2fcd85016e30774ab41d4ba4853cf70886
Sixten Hilborn <hilborn90@gmail.com>
2017-05-26 14:31:13 +0200

    Generate MT*

--- build.py
+++ build.py
@@ -3,6 +3,6 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds(shared_option_name="libiconv:shared", pure_c=True)
+    builder.add_common_builds(shared_option_name="libiconv:shared", pure_c=False)
     builder.run()
 
